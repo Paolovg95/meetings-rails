0a8c587207a591dc7f4114e1187129ee86b24c08
Paolo Vargas De Gasperi <paolo9517@gmail.com>
2022-03-23 11:43:26 -0300

    Home View and Bootsrap Added

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,12 @@
 class MeetingsController < ApplicationController
 
   def index
+    @meetings = Meeting.all
+  end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+    @meetingSlot = MeetingSlot.new
   end
 
 end
--- app/models/meeting_slot.rb
+++ app/models/meeting_slot.rb
@@ -1,3 +1,4 @@
 class MeetingSlot < ApplicationRecord
+  LENGHTS = [15,30,45,60]
   belongs_to :meeting
 end
